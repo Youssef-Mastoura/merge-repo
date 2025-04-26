1810f3398ee70d52810fe479efa271ddcf0e09e2
youssef <youssef.mastoura@esprit.tn>
2025-04-26 08:19:18 +0100

    add merging component

--- skillswappevent/src/app/app-routing.module.ts
+++ skillswappevent/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { EventComponent } from './event/event.component';
 import { AddeventComponent } from './addevent/addevent.component';
 import { ModifyeventComponent } from './modifyevent/modifyevent.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { MergingComponent } from './merging/merging.component';
 
 const routes: Routes = [
   { path: '', component: EventComponent },
   { path: 'addevent', component: AddeventComponent }, 
   { path: 'modifyevent/:id', component: ModifyeventComponent },
   { path: 'feedback/:id', component: FeedbackComponent },
+  { path: 'merge', component: MergingComponent },
+
 
 ];
 @NgModule({
